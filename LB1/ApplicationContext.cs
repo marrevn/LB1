8254b0242838aaf88e16f52ef87b7bff8c56aa3b
marrevn <sveta.i.volhina@gmail.com>
2025-03-27 13:53:15 +0500

    Добавление миграции

--- LB1/ApplicationContext.cs
+++ LB1/ApplicationContext.cs
@@ -6,10 +6,7 @@ namespace LB1
     {
         public DbSet<User> Users { get; set; } = null!;
 
-        public ApplicationContext()
-        {
-            Database.EnsureCreated();
-        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=usersdb;Username=postgres;Password=1111");
